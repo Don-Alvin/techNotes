e55715ba024471f0cbf1f0b7f467ea9064fe0cd2
Don Alvin <alvindon41@gmail.com>
2023-03-09 11:17:15 +0300

    Other CRUD operations

--- client/src/features/users/usersApiSlice.js
+++ client/src/features/users/usersApiSlice.js
@@ -6,8 +6,8 @@ const usersAdapter = createEntityAdapter({});
 const initialState = usersAdapter.getInitialState();
 
 export const usersApiSlice = apiSlice.injectEndpoints({
-	endpoints: (builders) => ({
-		getUsers: builders.query({
+	endpoints: (builder) => ({
+		getUsers: builder.query({
 			query: () => "/users",
 			validateStatus: (response, result) => {
 				return response.status === 200 && !result.isError;
@@ -28,6 +28,34 @@ export const usersApiSlice = apiSlice.injectEndpoints({
 					];
 				} else return [{ type: "User", id: "LIST" }];
 			},
+		}),
+		addNewUser: builder.mutation({
+			query: (initialUserData) => ({
+				url: "/users",
+				method: "POST",
+				body: {
+					...initialUserData,
+				},
+			}),
+			invalidatesTags: [{ type: "User", id: "LIST" }],
+		}),
+		updateUser: builder.mutation({
+			query: (initialUserData) => ({
+				url: "/users",
+				method: "PATCH",
+				body: {
+					...initialUserData,
+				},
+			}),
+			invalidatesTags: (result, error, arg) => [{ type: "User", id: arg.id }],
+		}),
+		deleteUser: builder.mutation({
+			query: ({ id }) => ({
+				url: `/users`,
+				method: "DELETE",
+				body: { id },
+			}),
+			invalidatesTags: (result, error, arg) => [{ type: "User", id: arg.id }],
 		}),
 	}),
 });
