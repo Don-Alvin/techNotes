8f843df9281eeac7bcafae0ff52709336dbdfb85
Don Alvin <alvindon41@gmail.com>
2023-03-13 15:12:39 +0300

    Persist Login

--- client/src/components/DashFooter.jsx
+++ client/src/components/DashFooter.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import {faHouse} from '@fortawesome/free-solid-svg-icons'
 import { useLocation, useNavigate } from 'react-router-dom'
+import useAuth from '../hooks/useAuth'
 
 const DashFooter = () => {
+
+    const {username, status} = useAuth()
 
     const navigate = useNavigate()
     const {pathname} = useLocation()
@@ -26,8 +29,8 @@ const DashFooter = () => {
   return (
     <footer className="dash-footer">
         {goHomeButton}
-        <p>Current User:</p>
-        <p>Status:</p>
+        <p>Current User:{username}</p>
+        <p>Status:{status}</p>
     </footer>
   )
 }
